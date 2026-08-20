f12dfab19f347719edad0300f27a1fde2d5ca556
Jonathanvictor-dev <jhonathanvictor142@gmail.com>
2026-08-20 10:32:06 -0300

    chore: adicionar configurações de CORS

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,25 @@ import blockedEmailRoutes from './routes/blocked-email.routes';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://jonathanvictor-dev.github.io'
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      };
+
+      return callback(
+        new Error(`Origin não permitida: ${origin}`),
+      );
+    }
+  }),
+);
+
 app.use(express.json());
 
 app.get('/', (_req, res) => {
